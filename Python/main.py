4e0f19f34856d30d24555ccca667962e3af11962
Tristan Roddis <tristanr@cogapp.com>
2021-07-29 12:53:13 +0100

    added basic Flask to serve a static file

--- Python/main.py
+++ Python/main.py
@@ -11,6 +11,17 @@
 from PIL import Image
 import urllib.request
 import requests
+from flask import Flask, request, send_from_directory
+
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='')
+
+@app.route("/")
+def send_file():
+    return send_from_directory('.', 'byte-fileO1375196.img')
+
+if __name__ == "__main__":
+    app.run()
 
 # This lets you filter object records by the collecting department in the museum that manages an object.
 req = requests.get('https://api.vam.ac.uk/v2/objects/search?id_collection=THES260586')
@@ -21,7 +32,7 @@ record_count = object_info["record_count"]
 print("There are {record_count} objects that are part of the 'Digital, Architecture and Design' department")
 print(object_records)
 
-for record in object_records:
+for record in object_records[0:1]:
     print(record['systemNumber'])
     URL = 'https://api.qrserver.com/v1/create-qr-code/?data=https://collections.vam.ac.uk/item/' + \
           record['systemNumber'] + '&size=200x200'
